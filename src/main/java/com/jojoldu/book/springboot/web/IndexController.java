1802fa0459987b3b85e8ef48dc0badb3d8228d44
harim <dlgkfla213@ajou.ac.kr>
2022-06-03 17:12:47 +0900

    ch5.4정보

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
@@ -18,9 +19,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){ // 서버 템플릿 엔진에서 사용할 수 있는 객체 저장 가능
+    public String index(Model model, @LoginUser SessionUser user){ // 서버 템플릿 엔진에서 사용할 수 있는 객체 저장 가능
+        //기존 httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선됨
+        //LoginUser 이용하면 세션 정보 가져올 수 있음
+
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //로그인성공 시 세션에 SessionUser 저장하도록 구성
 
         if(user != null){ //세션에 저장된 값이 있을 때 model에 userName으로 등록, 저장된 값없으면 로그인 버튼 보임
             model.addAttribute("loginUserName", user.getName());
